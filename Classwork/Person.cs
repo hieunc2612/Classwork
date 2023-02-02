1c68ea4e1708fa6b16c47585059b95bbb53a34aa
Hieu <nguyenconghieu@gmail.com>
2023-02-02 21:15:09 +0700

    Add project files.

---
+++ Classwork/Person.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Linq;
+
+namespace Classwork
+{
+    internal class Person
+    {
+        string name;
+        int age;
+        public string Name { get => name; set => name = value; }
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (value > 0) age = value;
+                else System.Console.WriteLine("Age cannot negative ");
+            }
+        }
+
+        //Auto Property
+        // public string Name { get; set; }
+        // public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + "; Age: " + Age;
+        }
+    }
+}
---
+++ Classwork/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classwork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.Name = "Nguyen Van A";
+            p.Age = 20;
+            System.Console.WriteLine(p);
+            Console.ReadLine();
+        }
+    }
+}
